8aca94937b2a2f7039cc72404031e982427b132d
Saad Amin <93635982+saad810@users.noreply.github.com>
2024-04-19 22:34:45 +0500

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Transition } from "@headlessui/react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import DropDown from "./DropDown";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const mobileNavRef = useRef(null);
@@ -10,7 +11,14 @@ const Navbar = () => {
     <header className="bg-regal-blue-700 text-regal-blue-50 sticky top-0">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between py-2 md:py-2">
-          <h1 className="text-xl md:text-2xl font-semibold">FaceCast Vote</h1>
+          <h1 className="text-xl md:text-2xl font-semibold">
+            <Link to="/">
+              <img
+                src={process.env.PUBLIC_URL + "/facecast_no_bg.png"}
+                alt="logo"
+              />
+            </Link>
+          </h1>
           <nav className="hidden lg:flex lg:items-center space-x-6 text-base font-medium">
             <Link to="/" className="hover:opacity-90">
               Home
@@ -63,7 +71,7 @@ const Navbar = () => {
               <Link to="/vote" className="hover:opacity-90">
                 VoteNow
               </Link>
-              <DropDown title={'More'}/>
+              <DropDown title={"More"} />
               <div className="flex gap-2">
                 {/* <DropDown title={'Muhammd Saad'} type={"userAccount"}/> */}
                 <button className="bg-regal-blue-900 text-regal-blue-50 px-3 py-2  rounded-md hover:bg-regal-blue-900/80">
